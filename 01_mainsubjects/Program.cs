423ea0647c5020e8f996657d6188444bb2dbbaf6
ihsankasim <akilsiztahta3132@gmail.com>
2024-11-13 22:07:54 +0300

    Add files via upload

---
+++ 01_mainsubjects/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_mainsubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region yazdırma komutları
+
+            //Console.WriteLine("merhaba dünya");
+
+            //Console.Write("nasılsın?");
+
+            #endregion
+
+            #region string değişkenler
+
+            //string
+            //değişken_türü değişken_adı
+            
+            //string customername;
+            //string customersurname;
+            //string customerphone;
+            //string customermail;
+            //string customerdistrict, city;
+
+            //customername = "dombik şaşkın";
+            //customersurname = "kedi";
+            //customerphone = "+90 300 750 00 00";
+            //customermail = "şaşkınlıdombik@gmail.com";
+            //customerdistrict = "sav kasabası";
+            //city = "ısparta";
+
+            //Console.WriteLine("     *****rezervasyon kartı*****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine("müşteri: " + customername + " " + customersurname);
+            //Console.WriteLine("tel no: " + customerphone);
+            //Console.WriteLine("mail: " + customermail);
+            //Console.WriteLine("adresi: " + customerdistrict + "/" + city);
+            //Console.WriteLine("------------------------------------------");
+
+
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
